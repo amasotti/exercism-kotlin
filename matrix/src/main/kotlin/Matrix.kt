3bbf69032e4ffbbd1c685a1b098e44ef92b7c148
Antonio Masotti <masotti@bikeleasing-service.de>
2023-05-28 17:32:58 +0200

    functional alternative to matrix problem

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -1,47 +1,56 @@
 class Matrix(private val matrixAsString: String) {
 
-    private val rows: List<List<Int>>
-    private val columns: List<List<Int>>
+    /**
+     * The rows and columns of the matrix as lists of integers.
+     */
+    private val rows: List<List<Int>> = extractRows()
+    private val columns: List<List<Int>> = rows.transpose()
 
-    init {
-        rows = extractRows()
-        columns = rows.transpose()
-    }
 
     private fun extractRows(): List<List<Int>> {
         return this.matrixAsString
             .lines()
-            .map { row -> row
-                .trim()
-                .split(" ")
-                .map { it.toInt() }
+            .map { row ->
+                row
+                    .trim()
+                    .split(" ")
+                    .map { it.toInt() }
             }
     }
 
 
+// First attempt at transpose
+//    private fun List<List<Int>>.transpose2(): List<List<Int>> {
+//        if (isEmpty()) return emptyList()
+//
+//        val nRows = first().size
+//        val nCols = size
+//
+//        val transposed = MutableList(nRows) { MutableList<Int>(nCols) { this[0][0] } }
+//
+//        for (i in 0 until nRows) {
+//            for (j in 0 until nCols) {
+//                transposed[i][j] = this[j][i]
+//            }
+//        }
+//
+//        return transposed
+//    }
+
+    // Second attempt: Kotlin idiomatic / functional programming style
     private fun List<List<Int>>.transpose(): List<List<Int>> {
-        if (isEmpty()) return emptyList()
-
-        val nRows = first().size
-        val nCols = size
-
-        val transposed = MutableList(nRows) { MutableList<Int>(nCols) { this[0][0] } }
-
-        for (i in 0 until nRows) {
-            for (j in 0 until nCols) {
-                transposed[i][j] = this[j][i]
-            }
+        return if (isEmpty()) emptyList()
+        else first().indices.map { col ->
+            map { row -> row[col] }
         }
-
-        return transposed
     }
 
 
-        fun column(colNr: Int): List<Int> {
-            return columns[colNr - 1]
-        }
+    fun column(colNr: Int): List<Int> {
+        return columns[colNr - 1]
+    }
 
-        fun row(rowNr: Int): List<Int> {
-            return rows[rowNr - 1]
-        }
+    fun row(rowNr: Int): List<Int> {
+        return rows[rowNr - 1]
+    }
 }
