7d0e4d5fd8eb7fc886bf8bb04ae829e34445a01d
Antonio Masotti <masotti@bikeleasing-service.de>
2023-05-28 14:15:03 +0200

    first solution hamming distance

---
+++ hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,19 @@
+object Hamming {
+
+    fun compute(leftStrand: String, rightStrand: String): Int {
+
+        require(leftStrand.length == rightStrand.length) { "left and right strands must be of equal length" }
+
+        if (leftStrand === rightStrand) return 0
+
+        var hammingDistance = 0
+        for (i in leftStrand.indices) {
+            if (leftStrand[i] != rightStrand[i]) {
+                hammingDistance++
+            }
+        }
+
+        return hammingDistance
+
+    }
+}
