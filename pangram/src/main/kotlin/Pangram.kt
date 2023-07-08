1761c93c923dbaa9551c4f3050359f1ea21dd1e5
amasotti <toniomasotti@gmail.com>
2023-07-08 21:20:54 +0200

    pangram solution

--- pangram/src/main/kotlin/Pangram.kt
+++ pangram/src/main/kotlin/Pangram.kt
@@ -1,6 +1,9 @@
 object Pangram {
 
+    const val ALPHABET_SIZE = 26
+
     fun isPangram(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val letters = input.toLowerCase().filter { it.isLetter() }.toSet()
+        return letters.size == ALPHABET_SIZE
     }
 }
