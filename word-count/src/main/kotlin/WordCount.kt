4ba899b2dac6ad62fdd3dc87a3fd5cdac8005af1
Antonio Masotti <masotti@bikeleasing-service.de>
2023-07-08 21:02:09 +0200

    second solution to word-count problem

--- word-count/src/main/kotlin/WordCount.kt
+++ word-count/src/main/kotlin/WordCount.kt
@@ -11,18 +11,16 @@ object WordCount {
      * contractions count as one word, all other chars are ignored
      */
     fun tokenize(phrase: String): List<String> {
-        val tokenRegex = """[a-zA-Z0-9']+""".toRegex()
+        val tokenRegex = Regex("""[a-zA-Z0-9']+""")
 
-        val tokens = mutableListOf<String>()
-
-        tokenRegex.findAll(phrase).forEach {
-            match -> tokens.add(match.value)
-        }
+        val tokens = tokenRegex.findAll(phrase).map {
+            match -> match.value
+        }.toList()
 
         return removeQuotation(tokens)
     }
 
-    fun removeQuotation(tokens: List<String>): List<String> = tokens.map { it.replace("^\'|^\"|\"$|\'$".toRegex(), "") }
+    fun removeQuotation(tokens: List<String>): List<String> =  tokens.map { it.replace(Regex("^['\"]|['\"]$"), "") }
 
 
     fun countTokenOccurrences(tokens: List<String>): Map<String, Int> = tokens.groupingBy { it }.eachCount()
