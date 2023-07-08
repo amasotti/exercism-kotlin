332bce94b67c159de421355112112cc6bec4a2d9
Antonio Masotti <masotti@bikeleasing-service.de>
2023-07-08 20:21:59 +0200

    solution for flatten-array

--- flatten-array/src/main/kotlin/Flattener.kt
+++ flatten-array/src/main/kotlin/Flattener.kt
@@ -1,5 +1,7 @@
 object Flattener {
-    fun flatten(source: Collection<Any?>): List<Any> {
-        TODO("Implement the function to complete the task")
+    fun flatten(source: Collection<Any?>): List<Any> = source.flatMap { it -> when(it) {
+       is Collection<*> -> flatten(it)
+        else -> listOfNotNull(it)
+    }
     }
 }
