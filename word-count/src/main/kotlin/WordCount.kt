b6a32ec979693513039625e6b23f1ba8ffbad3e2
Antonio Masotti <masotti@bikeleasing-service.de>
2023-07-08 20:58:39 +0200

    first solution to word-count problem

--- word-count/src/main/kotlin/WordCount.kt
+++ word-count/src/main/kotlin/WordCount.kt
@@ -1,6 +1,31 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        TODO("Implement this function to complete the task")
+        val tokens = tokenize(phrase.lowercase())
+        return countTokenOccurrences(tokens)
     }
+
+
+    /**
+     * Tokenize the phrase. According to the task description
+     * contractions count as one word, all other chars are ignored
+     */
+    fun tokenize(phrase: String): List<String> {
+        val tokenRegex = """[a-zA-Z0-9']+""".toRegex()
+
+        val tokens = mutableListOf<String>()
+
+        tokenRegex.findAll(phrase).forEach {
+            match -> tokens.add(match.value)
+        }
+
+        return removeQuotation(tokens)
+    }
+
+    fun removeQuotation(tokens: List<String>): List<String> = tokens.map { it.replace("^\'|^\"|\"$|\'$".toRegex(), "") }
+
+
+    fun countTokenOccurrences(tokens: List<String>): Map<String, Int> = tokens.groupingBy { it }.eachCount()
+
+
 }
