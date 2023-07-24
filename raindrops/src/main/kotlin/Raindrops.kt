2daf17f49021c724061130b29885ad7229a33c7b
Antonio Masotti <toniomasotti@gmail.com>
2023-07-24 18:43:46 +0200

    solve raindrops problem

--- raindrops/src/main/kotlin/Raindrops.kt
+++ raindrops/src/main/kotlin/Raindrops.kt
@@ -1,6 +1,7 @@
 object Raindrops {
-
     fun convert(n: Int): String {
-        TODO("Implement this function to complete the task")
+        val factors = listOf(3 to "Pling", 5 to "Plang", 7 to "Plong")
+        val result = factors.filter { (factor, _) -> n % factor == 0 }.map { it.second }.joinToString("")
+        return if (result.isEmpty()) n.toString() else result
     }
 }
