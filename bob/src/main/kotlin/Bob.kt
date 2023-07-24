3dcf3af7b6db2182a8aa25118b3b9b1cde28c2de
Antonio Masotti <toniomasotti@gmail.com>
2023-07-24 18:29:41 +0200

    solve Bob exercise

--- bob/src/main/kotlin/Bob.kt
+++ bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,22 @@
 object Bob {
+    /**
+     * Respond based on the input type.
+     * @param input the input to respond to
+     * @return the response
+     */
     fun hey(input: String): String {
-        TODO("Implement the function to complete the task")
+        val trimmedInput = input.trim()
+
+        return when {
+            trimmedInput.isEmpty() -> "Fine. Be that way!"
+            trimmedInput.isYelledQuestion() -> "Calm down, I know what I'm doing!"
+            trimmedInput.isShouting() -> "Whoa, chill out!"
+            trimmedInput.isQuestion() -> "Sure."
+            else -> "Whatever."
+        }
     }
 }
+
+fun String.isQuestion() = this.last() == '?'
+fun String.isShouting() = this.any { it.isLetter() } && this == this.uppercase()
+fun String.isYelledQuestion() = this.isQuestion() && this.isShouting()
