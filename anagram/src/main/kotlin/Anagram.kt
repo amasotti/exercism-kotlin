bd009a2a610dc7c8e8dcddf47612997d01297773
Antonio Masotti <toniomasotti@gmail.com>
2023-09-02 12:52:24 +0200

    solution for anagram

--- anagram/src/main/kotlin/Anagram.kt
+++ anagram/src/main/kotlin/Anagram.kt
@@ -1,16 +1,47 @@
+import kotlin.math.absoluteValue
+
 class Anagram(private val base : String) {
 
-    private val baseChars = base.toCharArray().map { it.lowercaseChar() }
-
+    /**
+     * Main Anagram function, called in the Unit Tests
+     */
     fun match(anagrams: Collection<String>): Set<String> {
         return anagrams.filter { isAnagram(it) }.toSet()
     }
 
-    fun isAnagram(word: String): Boolean {
+    private fun isAnagram(word: String): Boolean {
+        // If the length is different, it's not an anagram
         if (word.length != base.length) return false
-        if (word.equals(base, ignoreCase = true)) return false
-        val wordChars = word.toCharArray().map { it.lowercaseChar() }
-        return wordChars.all { baseChars.contains(it) } && wordChars.all { wordChars.count { c -> c == it } == baseChars.count { c -> c == it } }
+
+        // If the word is the same as the base, it's not an anagram
+        if (word.toLowerCase() == base.toLowerCase()) return false
+
+
+        // If the word has the same letters as the base, it's an anagram
+        return base
+            .measureDistanceFrom(word)
+            .isZero()
     }
+
+    /* -------------------- EXTENSIONS -------------------- */
+
+    /**
+     * Measures the distance between two strings in terms of characters not shared
+     * with the base string
+     */
+    private fun String.measureDistanceFrom(other: String): Int {
+
+        val baseWordCounts = this.toLowerCase().groupingBy { it }.eachCount()
+        val wordCounts = other.toLowerCase().groupingBy { it }.eachCount()
+
+        return baseWordCounts
+            .map { (letter, count) -> (count - (wordCounts[letter] ?: 0)).absoluteValue }
+            .sum()
+    }
+
+    /**
+     * Just a more human-readable check for zero
+     */
+    private fun Int.isZero() = this == 0
 
 }
